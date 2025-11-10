a48ca0d642fbddf33f7d7433a90f724a280d58f2
Nasjwa Eddyani <nasjwaeddyani@hotmail.com>
2025-11-10 19:59:45 +0100

    home page placeholder cards and layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
   def home
+    @tags = %w[Vegan Vegetarian Gluten-free Halal Kosher Dairy-free Low-salt]
+
+    # These can be empty; the view will show placeholders when empty
+    @nearby    = Food.limit(6)
+    @cooked    = Food.none
+    @groceries = Food.none
   end
 end
