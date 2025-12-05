d5a0320d9044c81b1cf5bcaba3dd0eed2a99b792
Nasjwa Eddyani <nasjwaeddyani@hotmail.com>
2025-12-05 12:37:53 +0100

    kitt checklist

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,28 +13,28 @@ Rails.application.configure do
   config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
 
-  # Ensures that a master key has been made available in ENV["RAILS_MASTER_KEY"], config/master.key, or an environment
-  # key such as config/credentials/production.key.
   # config.require_master_key = true
 
-  # Disable serving static files from `public/`, relying on NGINX/Apache to do so instead.
   # config.public_file_server.enabled = false
 
-  # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
   # Do not fall back to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
-  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "neighborfoodapp.com"
 
-  # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile"
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect"
 
   # Store uploaded files on Cloudinary.
   config.active_storage.service = :cloudinary
+
+  # config.action_cable.mount_path = nil
+  # config.action_cable.url = "wss://example.com/cable"
+  # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
+
+  # config.assume_ssl = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and secure cookies.
   config.force_ssl = true
@@ -47,28 +47,24 @@ Rails.application.configure do
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
 
-  # "info" includes generic and useful information about system operation.
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
 
-  # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
-  # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
   # config.active_job.queue_name_prefix = "neighborfood_production"
 
   config.action_mailer.perform_caching = false
 
-  # Enable locale fallbacks for I18n.
+  # config.action_mailer.raise_delivery_errors = false
+
   config.i18n.fallbacks = true
 
-  # Don't log any deprecations.
   config.active_support.report_deprecations = false
 
-  # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # Host header protection – allow naked + www.
+  # Allow both naked and www hosts
   config.hosts = [
     "neighborfoodapp.com",
     "www.neighborfoodapp.com"
